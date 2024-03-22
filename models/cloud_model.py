aecc580b752f8f41889fb2b748ca3edb7f36e011
galeyal2 <galeyal2@gmai.com>
2024-03-22 11:47:34 +0200

    test

--- models/cloud_model.py
+++ models/cloud_model.py
@@ -34,7 +34,7 @@ class CloudEventTemp(SqliteBase):
 class CloudEvent(SqliteBase):
     __tablename__ = 'cloud_events'
 
-    id = Column(Integer)
+    id = Column(Integer, autoincrement=True)
     event_id = Column(String(255), primary_key=True)
     request_id = Column(String(255))
     event_type = Column(String(255))
